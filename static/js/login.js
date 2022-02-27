15702046cb3f365f10db1e29ab1cb7d4801fda06
Eric Ma <mcr.988899@163.com>
2022-02-27 19:57:37 +0000

    fix login and register page

---
+++ static/js/login.js
@@ -0,0 +1,26 @@
+window.onload = function () {
+    if (localStorage.getItem("username") != null){
+        document.getElementById("name").value = localStorage.getItem("username")
+        document.getElementById("checkRememberName").checked = true
+    }
+}
+
+function validateForm(){
+
+    let nameInput = document.getElementById("name")
+    nameInput.parentElement.classList.add("was-validated")
+    let pwInput = document.getElementById("password")
+    pwInput.parentElement.classList.add("was-validated")
+
+    if (nameInput.value === '' || pwInput.value === '') {
+        return false
+    }
+
+    if (document.getElementById("checkRememberName").checked) {
+        localStorage.setItem("username", nameInput.value)
+    }else {
+        localStorage.removeItem("username")
+    }
+
+    return true
+}
--- static/js/register.js
+++ static/js/register.js
@@ -1,8 +1,39 @@
-function keyRules() {
-    const k = event.keyCode;   //48-57是大键盘的数字键，96-105是小键盘的数字键，8是退格符←
-    if ((k <= 57 && k >= 48) || (k <= 105 && k >= 96) || (k== 8)){
-        return true;
-    } else {
-        return false;
+
+function validateForm(){
+
+    let nameInput = document.getElementById("name")
+    let emailInput = document.getElementById("email")
+    let pwInput = document.getElementById("password")
+    let pwInput2 = document.getElementById("confirm-password")
+
+    nameInput.parentElement.classList.add("was-validated")
+    emailInput.parentElement.classList.add("was-validated")
+    pwInput.parentElement.classList.add("was-validated")
+    pwInput2.parentElement.classList.add("was-validated")
+
+    if (!checkEmail(emailInput.value)) {
+        emailInput.classList.add("is-invalid")
+        emailInput.parentElement.classList.remove("was-validated")
+    }else {
+        emailInput.classList.remove("is-invalid")
     }
+    if (pwInput2.value !== pwInput.value) {
+        pwInput2.classList.add("is-invalid")
+        pwInput2.parentElement.classList.remove("was-validated")
+    }else {
+        pwInput2.classList.remove("is-invalid")
+    }
+
+    if (nameInput.value === '' || emailInput.value === '' || !checkEmail(emailInput.value) ||
+        pwInput.value === '' || pwInput2.value !== pwInput.value) {
+        return false
+    }
+
+    return true
+}
+
+function checkEmail(emailInputValue){
+　　let myReg=/^[a-zA-Z0-9_-]+@([a-zA-Z0-9]+\.)+(com|cn|net|org)$/;
+
+　　return myReg.test(emailInputValue)
 }
