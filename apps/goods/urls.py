6e46440a276af23f1e50e3ca521817add8f9cdd8
Eric Ma <mcr.988899@163.com>
2022-03-08 16:39:55 +0000

    add addItem page

--- apps/goods/urls.py
+++ apps/goods/urls.py
@@ -3,4 +3,5 @@ from apps.goods import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('addItem', views.addItem, name='addItem')
 ]
--- apps/goods/views.py
+++ apps/goods/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'goods/index.html')
+
+def addItem(request):
+    return render(request, 'goods/addItem.html')
