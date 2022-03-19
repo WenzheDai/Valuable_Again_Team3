44de43e28f87ac645837a4df4b1f088f098c1cca
Eric Ma <mcr.988899@163.com>
2022-03-19 01:25:15 +0000

    fix login

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -8,7 +8,7 @@ from apps.user.views import RegisterView, LoginView, UserInfoView, \
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
 
-    path('login', LoginView.as_view(), name='login'),
+    path('login/', LoginView.as_view(), name='login'),
     path('logout', LogoutView.as_view(), name='logout'),
 
     path('', UserInfoView.as_view(), name='centerProfile'),
