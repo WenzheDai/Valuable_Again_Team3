aeb3125ff90478922787a13fe585220e0e124f7c
Wenzhe Dai <2628148d@student.gla.ac.uk>
2022-03-19 23:48:58 +0000

    modify debug avatar

--- apps/goods/models.py
+++ apps/goods/models.py
@@ -27,6 +27,4 @@ class ItemPicture(BaseModel):
         verbose_name = "ItemPicture"
         verbose_name_plural = verbose_name
 
-class HistoryRecord(BaseModel):
-    item = models.ForeignKey(Items, verbose_name='history', on_delete=None)
 
